ef6458b461cd1d8de49819df594ef673615112b0
Manfriny <manfriny@gmail.com>
2026-03-10 10:56:19 -0300

    Aula19 - Grass Signals

--- Game/Scripts/grass.gd
+++ Game/Scripts/grass.gd
@@ -3,10 +3,16 @@ extends Area2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var sprite_2d_back: Sprite2D = $Sprite2D_Back
 
-const  skewDuration := 1.5
+const  skewDuration = 1.5
+const  scaleDuration = 0.1
 
 var skewTween: Tween
 var skewTweenBack: Tween
+
+var scaleTween: Tween
+var startScale = Vector2(1.0,1.0)
+var endScale = Vector2(1.0,0.7)
+
 
 func _ready() -> void:
 	var startSkew = deg_to_rad(randf_range(-10,10))
@@ -24,3 +30,21 @@ func _ready() -> void:
 	skewTweenBack.tween_property(sprite_2d_back,"skew",endSkewBack,skewDuration).from(startSkewBack)
 	skewTweenBack.tween_property(sprite_2d_back,"skew",startSkewBack, skewDuration).from(endSkewBack)
 	skewTweenBack.set_ease(Tween.EASE_IN_OUT)
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	CreateNewScaleTween(endScale,scaleDuration)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	CreateNewScaleTween(startScale,scaleDuration * 3)
+
+
+func CreateNewScaleTween(targetValue: Vector2, duration: float):
+	if scaleTween and scaleTween.is_valid():
+		scaleTween.kill()
+	
+	scaleTween = get_tree().create_tween()
+	scaleTween.tween_property(sprite_2d,"scale",targetValue,duration)
+	scaleTween.set_trans(Tween.TRANS_BOUNCE)
+	scaleTween.set_ease(Tween.EASE_IN)
