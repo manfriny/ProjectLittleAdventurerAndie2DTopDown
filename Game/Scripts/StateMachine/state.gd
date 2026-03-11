ef0e4ff3b7e90323805faca3c99868bf11796576
Manfriny <manfriny@gmail.com>
2026-03-11 10:15:20 -0300

    Aula22 - Testing Current State

--- Game/Scripts/StateMachine/state.gd
+++ Game/Scripts/StateMachine/state.gd
@@ -5,10 +5,12 @@ var parentStateMachine : StateMachine
 
 @warning_ignore("unused_parameter")
 func UpdatePhysics(delta: float):
+	#print(name + " is updating physics")
 	pass
 
 
 func Update():
+	#print(name + " is updating")
 	pass
 
 
--- Game/Scripts/StateMachine/state_machine.gd
+++ Game/Scripts/StateMachine/state_machine.gd
@@ -1,3 +1,23 @@
 extends Node
-
 class_name StateMachine
+
+var currentState: State
+
+func _ready() -> void:
+	
+	for child in get_children():
+		var childState = child as State
+		childState.parentStateMachine = self
+		childState.Ready()
+	
+	#starta no state de indice zero, que é o primeiro item abaixo de StateMachine: IDLE
+	currentState = get_child(0)
+	currentState.Enter()
+
+
+func _physics_process(delta: float) -> void:
+	currentState.UpdatePhysics(delta)
+
+
+func _process(_delta: float) -> void:
+	currentState.Update()
