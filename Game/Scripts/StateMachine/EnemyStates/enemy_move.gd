866601ec59d792cedffb5d9ba71730729c05551b
Manfriny <manfriny@gmail.com>
2026-03-13 11:08:15 -0300

    Aula31 - Enemy Navigation Created

--- Game/Scripts/StateMachine/EnemyStates/enemy_move.gd
+++ Game/Scripts/StateMachine/EnemyStates/enemy_move.gd
@@ -1,1 +1,15 @@
 extends State
+
+@onready var navigation_agent_2d: NavigationAgent2D = $"../../NavigationAgent2D"
+
+var direction : Vector2
+
+func Update():
+	super.Update()
+	character.UpdateAnimation()
+	navigation_agent_2d.target_position = character.player.global_position
+	
+
+func UpdatePhysics(delta: float):
+	super.UpdatePhysics(delta)
+	direction = character.player.global_position.direction_to(navigation_agent_2d.get_next_path_position())
