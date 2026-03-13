42978b8402d0b1233992bf06f66ad4b97b5fee7e
Manfriny <manfriny@gmail.com>
2026-03-13 13:45:14 -0300

    Aula32 - Navigation Path Enemies

--- Game/Scripts/StateMachine/EnemyStates/enemy_move.gd
+++ Game/Scripts/StateMachine/EnemyStates/enemy_move.gd
@@ -1,15 +1,39 @@
 extends State
 
 @onready var navigation_agent_2d: NavigationAgent2D = $"../../NavigationAgent2D"
+@onready var player_pos_update_timer: Timer = $"../../PlayerPosUpdateTimer"
+
+const ACCELERATE = 5
 
 var direction : Vector2
+var speed = 100
+
 
 func Update():
 	super.Update()
 	character.UpdateAnimation()
-	navigation_agent_2d.target_position = character.player.global_position
+	
 	
 
 func UpdatePhysics(delta: float):
 	super.UpdatePhysics(delta)
-	direction = character.player.global_position.direction_to(navigation_agent_2d.get_next_path_position())
+	direction = character.global_position.direction_to(navigation_agent_2d.get_next_path_position())
+	
+	if navigation_agent_2d.is_target_reached() == false:
+		character.velocity = character.velocity.lerp(direction * speed, ACCELERATE * delta)
+		character.move_and_slide()
+		
+
+func _on_player_pos_update_timer_timeout() -> void:
+	navigation_agent_2d.target_position = character.player.global_position
+	
+
+func Enter():
+	super.Enter()
+	player_pos_update_timer.start()
+	
+
+func Exit():
+	super.Exit()
+	player_pos_update_timer.stop()
+	
